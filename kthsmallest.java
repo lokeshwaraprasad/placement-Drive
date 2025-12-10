e547fbab7402fc7369f119a8da67004395d1b00b
lokeshwaraprasad <141038939+lokeshwaraprasad@users.noreply.github.com>
2025-12-10 20:52:55 +0530

    Kth smallest.java

---
+++ kthsmallest.java
@@ -0,0 +1,36 @@
+/*
+Given an integer array nums and an integer k, return the kth largest element in the array.
+
+Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+
+Can you solve it without sorting?
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4
+ */
+
+//code(Function )
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+       PriorityQueue<Integer>heap=new PriorityQueue<>(Collections.reverseOrder());
+       for(int num:nums)
+       {
+        heap.add(num);
+        if(heap.size()>k)
+        {
+            heap.poll();
+        }
+       } 
+
+
+    return heap.peek();
+    }
+}
