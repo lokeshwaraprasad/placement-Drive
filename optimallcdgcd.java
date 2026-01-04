e839ccb8d420604ccdd04c46f43f3b1cb8e2e16f
lokeshwaraprasad <141038939+lokeshwaraprasad@users.noreply.github.com>
2026-01-04 22:32:24 +0530

    GCD LCD

---
+++ optimallcdgcd.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int lcm(int a, int b) {
+        return (a / gcd(a, b)) * b;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
