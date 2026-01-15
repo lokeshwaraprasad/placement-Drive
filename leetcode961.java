332d2b1b617c6d3a6fd1d71357e90a4814055e76
lokeshwaraprasad <141038939+lokeshwaraprasad@users.noreply.github.com>
2026-01-15 22:24:43 +0530

    N-Repeated Element in Size 2N Array

--- leetcode961.java
+++ leetcode961.java
@@ -1,3 +1,19 @@
-public class leetcode961 {
-    
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        int len = nums.length;
+        int arrsize = len / 2;
+
+        int[] freq = new int[10001];  // FIXED SIZE
+
+        for (int i = 0; i < len; i++) {
+            freq[nums[i]]++;
+        }
+
+        for (int i = 0; i < 10001; i++) {
+            if (freq[i] == arrsize) {
+                return i;
+            }
+        }
+        return -1; // FIXED missing return
+    }
 }
