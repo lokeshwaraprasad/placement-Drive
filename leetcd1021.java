b7db16ceba735348c60c79147dc5d2fb17330065
lokeshwaraprasad <141038939+lokeshwaraprasad@users.noreply.github.com>
2026-04-14 21:59:48 +0530

    Remove outer paranthesis

---
+++ leetcd1021.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String removeOuterParentheses(String s) {
+        Stack<Character> stack = new Stack<>();
+        StringBuilder result = new StringBuilder();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                if (!stack.isEmpty()) {
+                    result.append(ch);
+                }
+                stack.push(ch);
+            } else {
+                stack.pop();
+                if (!stack.isEmpty()) {
+                    result.append(ch);
+                }
+            }
+        }
+
+        return result.toString();
+    }
+}
